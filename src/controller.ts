8054ffd552747879d01b775e55e4c955d3c9010d
riswu <sugike0301@gmail.com>
2020-04-06 20:35:25 +0900

    Convert Indentation to Spaces

--- src/controller.ts
+++ src/controller.ts
@@ -2,44 +2,44 @@ import * as vscode from 'vscode';
 import Command from './command';
 
 export default class Controller {
-	private isInStickySelectionMode: boolean;
+  private isInStickySelectionMode: boolean;
 
-	constructor() {
-		this.isInStickySelectionMode = false;
-	}
+  constructor() {
+    this.isInStickySelectionMode = false;
+  }
 
-	async activate() {
-		await this.ensureContext();
-	}
+  async activate() {
+    await this.ensureContext();
+  }
 
-	async enterStickySelectionMode() {
-		this.isInStickySelectionMode = true;
-		await this.ensureContext();
-	}
+  async enterStickySelectionMode() {
+    this.isInStickySelectionMode = true;
+    await this.ensureContext();
+  }
 
-	async exitStickySelectionMode(editor: vscode.TextEditor, command?: Command) {
-		if (command !== undefined) {
-			await command.execute();
-		}
-		this.removeSelections(editor);
-		this.isInStickySelectionMode = false;
-		await this.ensureContext();
-	}
+  async exitStickySelectionMode(editor: vscode.TextEditor, command?: Command) {
+    if (command !== undefined) {
+      await command.execute();
+    }
+    this.removeSelections(editor);
+    this.isInStickySelectionMode = false;
+    await this.ensureContext();
+  }
 
-	removeSelections(editor: vscode.TextEditor) {
-		editor.selections = editor.selections.map(selection => {
-			const active = selection.active;
-			return new vscode.Selection(active, active);
-		});
-	}
+  removeSelections(editor: vscode.TextEditor) {
+    editor.selections = editor.selections.map(selection => {
+      const active = selection.active;
+      return new vscode.Selection(active, active);
+    });
+  }
 
-	async type(args: any) {
-		await vscode.commands.executeCommand("default:type", args);
-		this.isInStickySelectionMode = false;
-		await this.ensureContext();
-	}
+  async type(args: any) {
+    await vscode.commands.executeCommand("default:type", args);
+    this.isInStickySelectionMode = false;
+    await this.ensureContext();
+  }
 
-	async ensureContext() {
-		await vscode.commands.executeCommand('setContext', 'inStickySelectionMode', this.isInStickySelectionMode);
-	}
+  async ensureContext() {
+    await vscode.commands.executeCommand('setContext', 'inStickySelectionMode', this.isInStickySelectionMode);
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -3,94 +3,94 @@ import Controller from './controller';
 import Command from './command';
 
 export function activate(context: vscode.ExtensionContext) {
-	const controllers = new Map<string, Controller>();
+  const controllers = new Map<string, Controller>();
 
-	function getOrCreateController(editor: vscode.TextEditor) {
-		const key = editor.document.fileName;
-		if (controllers.has(key)) {
-			return controllers.get(key)!;
-		} else {
-			const controller = new Controller();
-			controllers.set(key, controller);
-			return controller;
-		}
-	}
+  function getOrCreateController(editor: vscode.TextEditor) {
+    const key = editor.document.fileName;
+    if (controllers.has(key)) {
+      return controllers.get(key)!;
+    } else {
+      const controller = new Controller();
+      controllers.set(key, controller);
+      return controller;
+    }
+  }
 
-	context.subscriptions.push(vscode.commands.registerCommand('sticky-selection.enterStickySelectionMode', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) return;
+  context.subscriptions.push(vscode.commands.registerCommand('sticky-selection.enterStickySelectionMode', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
 
-		const controller = getOrCreateController(editor);
-		controller.enterStickySelectionMode();
-	}));
+    const controller = getOrCreateController(editor);
+    controller.enterStickySelectionMode();
+  }));
 
-	context.subscriptions.push(vscode.commands.registerCommand('sticky-selection.exitStickySelectionMode', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) return;
+  context.subscriptions.push(vscode.commands.registerCommand('sticky-selection.exitStickySelectionMode', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
 
-		const controller = getOrCreateController(editor);
-		controller.exitStickySelectionMode(editor);
-	}));
+    const controller = getOrCreateController(editor);
+    controller.exitStickySelectionMode(editor);
+  }));
 
-	context.subscriptions.push(vscode.commands.registerCommand('type', args => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) return;
+  context.subscriptions.push(vscode.commands.registerCommand('type', args => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
 
-		const controller = getOrCreateController(editor);
-		controller.type(args);
-	}));
+    const controller = getOrCreateController(editor);
+    controller.type(args);
+  }));
 
-	vscode.window.onDidChangeActiveTextEditor(editor => {
-		if (!editor) return;
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+    if (!editor) return;
 
-		const controller = getOrCreateController(editor);
-		controller.activate();
-	});
+    const controller = getOrCreateController(editor);
+    controller.activate();
+  });
 
-	vscode.workspace.onDidCloseTextDocument(document => {
-		controllers.forEach((controller, key) => {
-			if (key === document.fileName) {
-				controllers.delete(key);
-			}
-		})
-	});
+  vscode.workspace.onDidCloseTextDocument(document => {
+    controllers.forEach((controller, key) => {
+      if (key === document.fileName) {
+        controllers.delete(key);
+      }
+    })
+  });
 
-	context.subscriptions.push(vscode.commands.registerCommand('sticky-selection.cursorLineStartSelect', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) return;
+  context.subscriptions.push(vscode.commands.registerCommand('sticky-selection.cursorLineStartSelect', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
 
-		editor.selections = editor.selections.map(selection => {
-			const active = selection.active;
-			return new vscode.Selection(selection.anchor, active.with(active.line, 0));
-		});
-	}));
+    editor.selections = editor.selections.map(selection => {
+      const active = selection.active;
+      return new vscode.Selection(selection.anchor, active.with(active.line, 0));
+    });
+  }));
 
-	interface ExitCommandSettings {
-		command: string;
-		args?: object;
-	}
+  interface ExitCommandSettings {
+    command: string;
+    args?: object;
+  }
 
-	function switchCommand(command: Command) {
-		context.subscriptions.push(vscode.commands.registerCommand(`sticky-selection.${command.name}`, () => {
-			const editor = vscode.window.activeTextEditor;
-			if (!editor) return;
+  function switchCommand(command: Command) {
+    context.subscriptions.push(vscode.commands.registerCommand(`sticky-selection.${command.name}`, () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) return;
 
-			console.log(command);
-			const controller = getOrCreateController(editor);
-			controller.exitStickySelectionMode(editor, command);
-		}));
-	}
+      console.log(command);
+      const controller = getOrCreateController(editor);
+      controller.exitStickySelectionMode(editor, command);
+    }));
+  }
 
-	const configuration = vscode.workspace.getConfiguration("sticky-selection");
-	const exitCommandSettings = configuration.get<Array<ExitCommandSettings>>("exitCommands");
-	if (exitCommandSettings) {
-		exitCommandSettings.forEach(settings => {
-			const command = new Command(settings.command, settings.args ? settings.args : null);
-			switchCommand(command);
-		})
-	}
+  const configuration = vscode.workspace.getConfiguration("sticky-selection");
+  const exitCommandSettings = configuration.get<Array<ExitCommandSettings>>("exitCommands");
+  if (exitCommandSettings) {
+    exitCommandSettings.forEach(settings => {
+      const command = new Command(settings.command, settings.args ? settings.args : null);
+      switchCommand(command);
+    })
+  }
 }
 
 export function deactivate() {
-	return;
+  return;
 }
